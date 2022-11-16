845bb29809a87170d1987d2ede1c5c4f594bfe7b
lolmem745 <93839006+lolmem745@users.noreply.github.com>
2022-11-17 01:15:12 +0300

    Extended solution for first problem

---
+++ N1_Extended/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = ArrMethods.createArr();
+ArrMethods.fillArray(array, 5, 10);
+ArrMethods.printArray(array);
+int res = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+    {
+        res++;
+    }
+}
+Console.WriteLine($"Количество четных чисел в массиве: {res}");
+Console.ReadKey(true);
