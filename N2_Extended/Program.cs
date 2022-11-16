3e5a8803780769f0454bb2e0bdd785a050f10c46
lolmem745 <93839006+lolmem745@users.noreply.github.com>
2022-11-17 01:22:46 +0300

    Extended solution for second problem

---
+++ N2_Extended/Program.cs
@@ -0,0 +1,9 @@
+﻿int[] array = ArrMethods.createArr();
+ArrMethods.fillArray(array);
+ArrMethods.printArray(array);
+int res = 0;
+for (int i = 0; i < array.Length; i = i + 2)
+{
+    res = res + array[i];
+}
+Console.WriteLine($"Сумма нечетных элементов в массиве: {res}");
